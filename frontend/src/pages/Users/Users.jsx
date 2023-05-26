921a6b22b8064a1255ddf6c33f948827f67346c2
Louis-Marie Michelin <lmichelin@outlook.fr>
2023-05-26 14:43:05 +0200

    move user fetching logic into custom hook

--- frontend/src/pages/Users/Users.jsx
+++ frontend/src/pages/Users/Users.jsx
@@ -1,31 +1,10 @@
 import './Users.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import AddUserForm from '../../components/AddUserForm/AddUserForm';
 import UsersTable from '../../components/UsersTable/UsersTable';
+import { useFetchUsers } from './useFetchUsers';
 
 function Users() {
-  const [users, setUsers] = useState([]);
-  const [usersLoadingError, setUsersLoadingError] = useState(null);
-
-  const fetchUsers = () => {
-    setUsersLoadingError(null);
-
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/users`)
-      .then((response) => {
-        setUsers(response.data.users);
-      })
-      .catch((error) => {
-        setUsersLoadingError('An error occured while fetching users.');
-        console.error(error);
-      });
-  };
-
-  // fetch users on component mount
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { users, usersLoadingError, fetchUsers } = useFetchUsers();
 
   return (
     <div className="Users-container">
