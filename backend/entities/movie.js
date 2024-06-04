9593cdb23d3ded2294c78c9884ce2ce5b8dbe32e
Anicito Xavier <xavier.anicito@student-cs.fr>
2024-06-04 15:21:32 +0200

    Mise à jour.

--- backend/entities/movie.js
+++ backend/entities/movie.js
@@ -3,13 +3,17 @@ import typeorm from 'typeorm' ;
 const Movie = new typeorm.EntitySchema({
 	name: 'Movie',
 	columns: {
-	  name: {
-		primary: true,
-		type: String,
-		generated: true,
-	  },
+		id : {
+			primary : true,
+			type : Number,
+			generated : true,
+		},
+		name: {
+			type: String,
+			unique : true,
+	  	},
 	  release_date: { type: String }
 	},
-  });
+});
   
-  export default Movie ;
+export default Movie ;
--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,4 +1,6 @@
 import express from 'express' ;
+import { appDataSource } from '../datasource.js' ;
+import Movie from '../entities/movie.js' ;
 
 const router = express.Router();
 
@@ -7,7 +9,24 @@ router.get('/', function (req, res) {
 })
 
 router.post('/new', function (req, res) {
-    console.log(req.body)
-})
+    const movie_repository = appDataSource.getRepository(Movie) ;
+	const new_movie = appDataSource.create({
+		name : req.body.name,
+		release_date : req.body.release_date 
+	}) ;
+
+	movie_repository
+		.save({new_movie})
+		.then(function (saved_movie) {res.status(201).json({
+			message : "Movie successfully saved .",
+			id : saved_movie.id,
+			name : saved_movie.name,
+			release_date : saved_movie.release_date
+		})})
+		.catch(function (error) {console.error(error) ;
+			if (error.code === '23505') {res.status(400).json({message: `Movie with name "${new_movie.name}" already exists.`}) ;} 
+			else {res.status(500).json({ message: 'Error while creating the movie.' }) ;}
+		}) ;
+}) ;
 
 export default router
